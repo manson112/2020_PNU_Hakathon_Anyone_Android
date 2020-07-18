73b6c843308dbba7584d0ab1faece8d31c370e9f
Kwanwoo Kim <rlarhksdn777@naver.com>
2020-07-18 15:32:25 +0900

    - LoginActivity.kt

---
+++ app/src/main/java/pnu/hakathon/anyone/controller/LoginActivity.kt
@@ -0,0 +1,17 @@
+package pnu.hakathon.anyone.controller
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import pnu.hakathon.anyone.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_login)
+    }
+}
