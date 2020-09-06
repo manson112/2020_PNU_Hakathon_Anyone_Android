93423ecdc8e07f58b5ec8e81c1bbea1c446f387d
Kwanwoo Kim <rlarhksdn777@naver.com>
2020-09-06 17:33:43 +0900

    - Unused resources are removed - HomeFragment's layout is changed from nestedscrollview to recyclerview

--- app/src/main/java/pnu/hakathon/anyone/ui/adapter/HomeFragmentListAdapter.kt
+++ app/src/main/java/pnu/hakathon/anyone/ui/adapter/HomeFragmentListAdapter.kt
@@ -1,60 +1,148 @@
 package pnu.hakathon.anyone.ui.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.home_viewtype_1.view.*
+import kotlinx.android.synthetic.main.home_viewtype_2.view.*
+import kotlinx.android.synthetic.main.home_viewtype_3.view.*
+import kotlinx.android.synthetic.main.home_viewtype_4.view.*
+import kotlinx.android.synthetic.main.home_viewtype_5.view.*
 import kotlinx.android.synthetic.main.item_home_list.view.*
 import pnu.hakathon.anyone.R
 import pnu.hakathon.anyone.entity.StoreModel
+import pnu.hakathon.anyone.ui.activity.SearchActivity
 import pnu.hakathon.anyone.ui.activity.StoreDetailActivity
 
 class HomeFragmentListAdapter internal constructor(
-    val context: Context
-) : RecyclerView.Adapter<HomeFragmentListAdapter.ViewHolder>() {
+    val context: Context, val categoryID: String, val categoryName: String, val frag: Fragment
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private val inflater: LayoutInflater = LayoutInflater.from(context)
     private var stores = emptyList<StoreModel>()
     private var onClickedTime = System.currentTimeMillis()
+    private var seats = 0
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            inflater.inflate(
-                R.layout.item_home_list,
-                parent,
-                false
-            )
-        ).apply {
-            itemView.setOnClickListener {
-                val position = adapterPosition.takeIf { it != RecyclerView.NO_POSITION } ?: return@setOnClickListener
-                val currentTime = System.currentTimeMillis()
-                if (currentTime - onClickedTime > itemView.home_fragment_item_transformationLayout.duration) {
-                    StoreDetailActivity.startActivity(
-                        itemView.home_fragment_item_transformationLayout,
-                        stores[position]
-                    )
-                    onClickedTime = currentTime
+    inner class ViewTypeOne(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ViewTypeTwo(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ViewTypeThree(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ViewTypeFour(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ViewTypeFive(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        when(viewType) {
+            0 -> {
+                return ViewTypeOne(inflater.inflate(R.layout.home_viewtype_1, parent, false)).apply {
+                    Glide.with(context)
+                    .load("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png")
+                    .into(itemView.home_profile_image)
                 }
+            }
+            1 -> {
+                return ViewTypeTwo(inflater.inflate(R.layout.home_viewtype_2, parent, false)).apply {
+                    itemView.home_searchbar.hint = "$categoryName 이름을 검색해보세요"
+                    itemView.home_searchbar.setOnClickListener {
+                        context.startActivity(Intent(context, SearchActivity::class.java).putExtra("categoryID", categoryID))
+                    }
+                }
+            }
+            2 -> {
+                return ViewTypeThree(inflater.inflate(R.layout.home_viewtype_3, parent, false)).apply {
+                    itemView.home_text_category_1.text = categoryName
+                    itemView.home_text_category_2.text = categoryName
+                }
+            }
+            3 -> {
+                return ViewTypeFour(inflater.inflate(R.layout.home_viewtype_4, parent, false)).apply {
+                    val tabAdapter = TabAdapter(frag)
+                    itemView.home_hash_pager.adapter = tabAdapter
+                    TabLayoutMediator(itemView.home_tablayout, itemView.home_hash_pager) { tab, position ->
+                        when(position) {
+                            0 -> tab.text = "#조용한"
+                            1 -> tab.text = "#쾌적한"
+                            2 -> tab.text = "#활기찬"
+                            3 -> tab.text = "#친절한"
+                        }
+                    }.attach()
+                    itemView.home_hash_pager.isUserInputEnabled = false
+                }
+            }
+            4 -> {
+                return ViewTypeFive(inflater.inflate(R.layout.home_viewtype_5, parent, false)).apply {
+                    itemView.home_text_category_3.text = categoryName
+                }
+            }
+            5 -> {
+                return ViewHolder(
+                    inflater.inflate(
+                        R.layout.item_home_list,
+                        parent,
+                        false
+                    )
+                ).apply {
+                    itemView.setOnClickListener {
+                        val position = adapterPosition.takeIf { it != RecyclerView.NO_POSITION } ?: return@setOnClickListener
+                        val currentTime = System.currentTimeMillis()
+                        if (currentTime - onClickedTime > itemView.home_fragment_item_transformationLayout.duration) {
+                            StoreDetailActivity.startActivity(
+                                itemView.home_fragment_item_transformationLayout,
+                                stores[position]
+                            )
+                            onClickedTime = currentTime
+                        }
+                    }
+                }
+            }
+            else -> {
+                return ViewHolder(
+                    inflater.inflate(
+                        R.layout.item_home_list,
+                        parent,
+                        false
+                    )
+                )
             }
         }
     }
-
     override fun getItemCount(): Int = stores.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val current = stores[position]
-        Glide.with(context).load(current.imageURL).placeholder(R.drawable.image_empty).into(holder.itemView.home_fragment_item2_image)
-        holder.itemView.home_fragment_item2_text.text = current.storeName
-        holder.itemView.home_fragment_item2_address.text = current.address
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ViewTypeOne) {
+            holder.itemView.home_text_num_of_seat.text = seats.toString()
+        } else if (holder is ViewHolder) {
+            val current = stores[position]
+            Glide.with(context).load(current.imageURL).placeholder(R.drawable.image_empty).into(holder.itemView.home_fragment_item2_image)
+            holder.itemView.home_fragment_item2_text.text = current.storeName
+            holder.itemView.home_fragment_item2_address.text = current.address
 
-        val percent = (current.current / current.total.toDouble() * 100).toInt()
-        holder.itemView.home_fragment_item_battery_percent.text = "$percent%"
+            val percent = (current.current / current.total.toDouble() * 100).toInt()
+            holder.itemView.home_fragment_item_battery_percent.text = "$percent%"
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position < 5) {
+            return position
+        }
+        return 5
     }
 
     internal fun setList(ns: List<StoreModel>) {
-        this.stores = ns
-        notifyDataSetChanged()
+        val arr = arrayListOf<StoreModel>()
+        for (i in 0 until 5) { arr.add(0, StoreModel()) }
+        arr.addAll(ns)
+        this.stores = arr
+
+        var seats = 0
+        ns.forEach { item -> seats += (item.total - item.current) }
+
+        notifyItemChanged(4)
+        notifyItemRangeChanged(5, itemCount)
     }
 }
--- app/src/main/java/pnu/hakathon/anyone/ui/fragment/HomeFragment.kt
+++ app/src/main/java/pnu/hakathon/anyone/ui/fragment/HomeFragment.kt
@@ -1,22 +1,16 @@
 package pnu.hakathon.anyone.ui.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DividerItemDecoration
-import com.bumptech.glide.Glide
-import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_home.view.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import pnu.hakathon.anyone.R
-import pnu.hakathon.anyone.ui.activity.SearchActivity
+import pnu.hakathon.anyone.entity.StoreModel
 import pnu.hakathon.anyone.ui.adapter.HomeFragmentListAdapter
-import pnu.hakathon.anyone.ui.adapter.TabAdapter
 import pnu.hakathon.anyone.viewmodel.MainViewModel
 
 class HomeFragment : Fragment(){
@@ -26,48 +20,11 @@ class HomeFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         val v = inflater.inflate(R.layout.fragment_home, container, false)
-
-        Glide.with(container?.context!!)
-            .load("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png")
-            .into(v.home_profile_image)
-
-        v.home_text_category_1.text = mainViewModel.categoryName
-        v.home_text_category_2.text = mainViewModel.categoryName
-        v.home_text_category_3.text = mainViewModel.categoryName
-        v.home_searchbar.hint = "${mainViewModel.categoryName} 이름을 검색해보세요"
-
-        val tabAdapter = TabAdapter(this)
-        v.home_hash_pager.adapter = tabAdapter
-        TabLayoutMediator(v.home_tablayout, v.home_hash_pager) { tab, position ->
-            when(position) {
-                0 -> tab.text = "#조용한"
-                1 -> tab.text = "#쾌적한"
-                2 -> tab.text = "#활기찬"
-                3 -> tab.text = "#친절한"
-            }
-        }.attach()
-        v.home_hash_pager.isUserInputEnabled = false
-
-        v.home_searchbar.setOnClickListener {
-            startActivity(Intent(requireActivity(), SearchActivity::class.java).putExtra("categoryID", mainViewModel.categoryID))
-        }
-
-        val adapter =
-            HomeFragmentListAdapter(container.context)
+        val adapter = HomeFragmentListAdapter(container?.context!!, mainViewModel.categoryID, mainViewModel.categoryName, this)
         v.home_recommend_recyclerview.adapter = adapter
-        val itemDecorator = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
-        ContextCompat.getDrawable(container.context, R.drawable.divider_home_list)?.let {
-            itemDecorator.setDrawable(
-                it
-            )
-        }
-        v.home_recommend_recyclerview.addItemDecoration(itemDecorator)
         mainViewModel.stores.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.setList(it)
-                var seats = 0
-                it.forEach { item -> seats += (item.total - item.current) }
-                v.home_text_num_of_seat.text = seats.toString()
+                adapter.setList(it as MutableList<StoreModel>)
                 v.home_empty_text.visibility = if (it.isEmpty()) View.VISIBLE else View.GONE
             }
         })
